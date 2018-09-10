340cb1120e0782a333ee4cf715ad46f069bb32a6
literale <43133944+literale@users.noreply.github.com>
2018-09-10 15:13:04 +0600

    Initial commit

---
+++ Tree1/Form1.cs
@@ -0,0 +1,212 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Tree1
+{
+    public partial class Form1 : Form
+    {
+        MyTree myTree = new MyTree();
+        
+        bool drawing = false;
+        bool flag = false;
+        public Form1()
+        {
+            InitializeComponent();
+            btnAdd.Enabled = false;
+            btnDelete.Enabled = false;
+            btnFind.Enabled = false;
+            BtnSortAr.Enabled = false;
+            btnSort.Enabled = false;
+        }
+
+        private void btnCreate_Click(object sender, EventArgs e)//СОздать дерево
+        {
+            //int L = richTextBox1.Lines.Count();
+            //for(int i =0; i<L; i++)
+            //{
+            //    if (richTextBox1.Lines[i]!="")
+            //    {
+            //        int k = Convert.ToInt32(richTextBox1.Lines[i]);
+            //        myTree.Insert(ref myTree.top, k, 200, 40);
+
+            //    }
+            //}
+             myTree = new MyTree();
+
+            Random r = new Random();
+            for (int i = 0; i<1000; i++)
+            {
+               // myTree.k = 15;
+                int k = r.Next(0, 100);
+                myTree.Insert(ref myTree.top, k, 200, 40);
+            }
+
+            myTree.g = this.CreateGraphics();
+            myTree.g.Clear(Color.WhiteSmoke);
+            myTree.DrawNode(myTree.top);
+            myTree.Array();
+            myTree.SetStrSort(myTree.top);
+            myTree.FindNode(myTree.top, 200, 200);
+            richTextBox1.Text = "";
+            for (int i = 0; i < 100; i++)
+            {
+                richTextBox1.Text += Convert.ToString(i) + " : " + Convert.ToString(myTree.array[i]) + "\r\n";
+            }
+            richTextBox1.Enabled = true;
+
+            //btnAdd.Enabled = true;
+            //btnDelete.Enabled = true;
+            //btnFind.Enabled = true;
+            //BtnSortAr.Enabled = true;
+        }
+
+        void MyDraw()
+        {
+
+        }
+
+        private void Form1_MouseDown(object sender, MouseEventArgs e)//щелк
+        {
+            if (myTree.selectNode!=null)
+            myTree.selectNode.visit = false;
+            myTree.DeSelect(myTree.top);
+            myTree.selectNode = myTree.FindNode(myTree.top, e.X, e.Y);
+             drawing = (myTree.selectNode != null);
+            if (drawing)
+            {
+                myTree.selectNode.visit = true;
+                flag = true;
+            }
+        }
+
+        private void Form1_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (flag)
+            {
+                if (drawing)
+                    myTree.Delta(myTree.selectNode, myTree.selectNode.x - e.X, myTree.selectNode.y - e.Y);
+                else
+                {
+                    myTree.DeSelect(myTree.top);
+                    myTree.selectNode = myTree.FindNode(myTree.top, e.X, e.Y);
+                    if (myTree.selectNode != null)
+                        myTree.selectNode.visit = true;
+                }
+             //   myTree.g.Clear(Color.WhiteSmoke);
+              //  myTree.g = this.CreateGraphics();
+                myTree.DrawNode(myTree.top);
+                richTextBox1.Text = "";
+
+
+                myTree.g.Clear(Color.WhiteSmoke);
+                myTree.g = this.CreateGraphics();
+                myTree.DrawNode(myTree.top);
+            }
+        }//Двиг
+
+        private void Form1_MouseUp(object sender, MouseEventArgs e)
+        {
+            if (flag)
+            {
+                drawing = false;
+                flag = false;
+            }
+        }//Разщелк
+
+        private void BtnSortAr_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                richTextBox1.Text = "";
+                //richTextBox1.Text = myTree.SetStrSort(myTree.top);
+            }
+            catch { }
+        }
+
+        private void btnEnd_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void btnDelete_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                int data = Convert.ToInt32(tbDelete.Text);
+                myTree.Delete(data, ref myTree.top);
+                myTree.g.Clear(Color.WhiteSmoke);
+                myTree.g = this.CreateGraphics();
+                myTree.DrawNode(myTree.top);
+            }
+            catch
+            { }
+        }
+
+        private void btnAdd_Click(object sender, EventArgs e)
+        { try
+            {
+                int data = Convert.ToInt32(tbAdd.Text);
+                myTree.Insert(ref myTree.top, data, myTree.top.x, myTree.top.y);
+                myTree.g.Clear(Color.WhiteSmoke);
+                myTree.g = this.CreateGraphics();
+                myTree.DrawNode(myTree.top);
+            }
+            catch
+            { }
+        }
+
+        private void btnFind_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                int data = Convert.ToInt32(tbFind.Text);
+                if (myTree.selectNode != null)
+                    myTree.selectNode.visit = false;
+                myTree.selectNode = myTree.FindNodeVal(data, myTree.top);
+                if (myTree.selectNode != null)
+                    myTree.selectNode.visit = true;
+                myTree.g.Clear(Color.WhiteSmoke);
+                myTree.g = this.CreateGraphics();
+                myTree.DrawNode(myTree.top);
+            }
+            catch
+            { }
+        }
+
+        private void btnSort_Click(object sender, EventArgs e)
+        {
+            
+        //    List<int> val = new List<int>();
+        //    if (myTree.selectNode != null)
+        //        myTree.selectNode.visit = true;
+        //    //while(myTree.top!=null)
+        //    //{
+        //    //    Node node = myTree.Competition(ref myTree.top);
+        //    //    if (node!=null)
+        //    //    val.Add(Convert.ToInt32(node.data));
+
+        //    //}
+        //    myTree.Competition(ref myTree.top);
+        //    myTree.g.Clear(Color.WhiteSmoke);
+        //    myTree.g = this.CreateGraphics();
+
+            
+        //    for (int i = 0; i < val.Count; i++)
+        //    {
+
+        //        int k = val[i];
+        //            myTree.Insert(ref myTree.top, k, 200, 40);
+
+                
+        //    }
+        //myTree.DrawNode(myTree.top);
+        }
+    }
+}
---
+++ Tree1/MyTree.cs
@@ -0,0 +1,218 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+
+namespace Tree1
+{
+   public class MyTree
+    {
+        public Node top;
+        public Node selectNode = null;
+        public Bitmap bitmap;
+        public  Node node;
+       public int k = 1;
+       public int[] array = new int[100];
+
+        
+        const int step = 20;
+        const int Geom = 1;
+        Node q;
+       public Graphics g;
+        Pen MyPen = new Pen(Color.Black);
+        SolidBrush MyBrush;
+        Font MyFont = new Font("Arial",7);
+        public void Array()
+        {
+            for (int i = 0; i < 100; i++)
+            {
+                array[i] = 0;
+            }
+        }
+        public void Search(int val)//Поиск и вставка
+        { }
+        public Node FindNode (Node p, int x, int y)//Поиск по координатам
+        {
+
+            Node result = null;
+            if (p == null)
+                return result;
+            if (((p.x - x) * (p.x - x) + (p.y - y) * (p.y - y)) < 100)
+                result = p;
+            else
+            {
+                result = FindNode(p.left, x, y);
+                if (result == null)
+                    result = FindNode(p.right, x, y);
+            }
+
+            return result;
+        }
+        public Node FindNodeVal(int val, Node p)//Поиск по значению
+        {
+            Node result = null;
+
+            if (p == null)
+                return result;
+            if (Convert.ToInt32(p.data) == val)
+                result = p;
+            else
+            {
+                result = FindNodeVal(val, p.left);
+                if (result == null)
+                    result = FindNodeVal(val, p.right);
+            }
+
+            return result;
+        }
+        public void Insert (ref Node t,object data, int x, int y)//Вставить узел
+        {
+            if (t == null)
+            {
+                t = new Node(null, null, data, x, y);
+                t.count++;
+            }
+            else
+            {
+                if (Convert.ToInt32(data) < Convert.ToInt32(t.data))
+                {
+                  //  k--;
+                    Insert(ref t.left, data, t.x - step*k, t.y + step);
+                    
+                }
+                else
+                    if (Convert.ToInt32(data) > Convert.ToInt32(t.data))
+                {
+                  //  k--;
+                    Insert(ref t.right, data, t.x + step*k, t.y + step);
+                    
+                }
+                else
+                    t.count++;
+            }
+
+        }
+        public void DeSelect(Node p)//Снять выбор
+        { }
+        public void Delta(Node p, int dx, int dy)//смещение дерева
+        {
+            p.x -= dx; p.y -= dy;
+            if (p.left != null)
+                Delta(p.left, dx, dy);
+            if (p.right != null)
+                Delta(p.right,dx, dy);
+        }
+        public void Delete(int data, ref Node tree)//Удалить узел
+        {
+            if (tree!= null)
+            {
+                if (data < Convert.ToInt32(tree.data))
+                    Delete(data, ref tree.left);
+                else
+                {
+                    if (data > Convert.ToInt32(tree.data))
+                        Delete(data, ref tree.right);
+                    else
+                    {
+                        q.count--;
+                        q = tree;
+                        if (q.right == null)
+                            tree = q.left;
+                        else
+                        {
+                            if (q.left == null)
+                                tree = q.right;
+                            else
+                                Del(ref q.left);
+                        }
+                    }
+                }
+            }
+        }
+        void Del(ref Node r)
+        {
+            if (r.right != null)
+                Del(ref r.right);
+            else
+            { q.data = r.data; q = r; r = r.left;}
+        }
+        public void DrawNode(Node p)//НАрисовать дерево
+        {
+            try
+            {
+                int R = 12;
+                if (p.left != null)
+                    g.DrawLine(MyPen, p.x, p.y, p.left.x, p.left.y);
+                if (p.right != null)
+                    g.DrawLine(MyPen, p.x, p.y, p.right.x, p.right.y);
+                if (p.visit)
+                    MyBrush = (SolidBrush)Brushes.Yellow;
+                else
+                    MyBrush = (SolidBrush)Brushes.LightYellow;
+                g.FillEllipse(MyBrush, p.x - R, p.y - R, 2 * R, 2 * R);
+                string s = Convert.ToString(p.data)+" : "+ Convert.ToString(p.count);
+                SizeF size = g.MeasureString(s, MyFont);
+                g.DrawString(s, MyFont, Brushes.Black, p.x - size.Width / 2, p.y - size.Height / 2);
+                if (p.left != null)
+                    DrawNode(p.left);
+                if (p.right != null)
+                    DrawNode(p.right);
+            }
+            catch
+            {
+                
+            }
+
+        }
+        public void SetStrSort(Node p)
+        {
+            
+            if (p == null)
+            { }
+            if (p.right != null)
+            {
+                SetStrSort(p.right);
+            }
+            int i = Convert.ToInt32(p.data);
+            int k = p.count;
+            array[i] = k;
+            if (p.left != null)
+            {
+                SetStrSort(p.left);
+            }
+            
+           
+
+        }
+        public Node Competition(ref Node ph)
+        {
+            if (ph.left != null)
+            {
+                if (ph.left.data == ph.data)
+                    ph.left = Competition(ref ph.left);
+                else
+                    if (ph.right != null)
+                    ph.right = Competition(ref ph.right);
+
+            }
+            else
+            {
+                if (ph.right != null)
+                    ph.right = Competition(ref ph.right);
+            }
+            if ((ph.left == null) && (ph.right == null))
+                ph = null;
+            else
+            {
+                if ((ph.left == null) || ((ph.right != null) && (Convert.ToInt32(ph.left.data) > Convert.ToInt32(ph.right.data))))
+                    ph.data = ph.right.data;
+                else
+                    ph.data = ph.left.data;
+                return ph;
+            }
+            return ph;
+        }
+    }
+}
